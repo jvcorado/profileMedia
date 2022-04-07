6d6a5d26ef18d0f272591ddd0807b5d67b681744
João Victor Corado <jhonisilva545@gmail.com>
2022-04-07 12:36:10 -0300

    index

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function dark(){
+    document.querySelector("body").classList.remove('bg-light')
+    document.querySelector("body").classList.remove('text-dark')
+    document.querySelector("body").classList.add('bg-dark')
+    document.querySelector("body").classList.add('text-white')
+    const variasClasses = document.querySelectorAll(".col-sm-5")
+    const link = document.querySelectorAll(".col-sm-5 a")
+
+    variasClasses.forEach(Element =>{
+        Element.classList.add('border')
+        Element.classList.add('border-4')
+        Element.classList.add('border-light')
+
+    })
+
+    link.forEach(Element =>{
+        Element.classList.remove('text-dark')
+        Element.classList.add('text-white')
+    })
+    
+} 
+function white(){
+    document.querySelector("body").classList.remove('bg-dark')
+    document.querySelector("body").classList.remove('text-white')
+    document.querySelector("body").classList.add('bg-ligth')
+    document.querySelector("body").classList.add('text-dark')
+    const link = document.querySelectorAll(".col-sm-5 a")
+
+    const variasClasses = document.querySelectorAll(".col-sm-5")
+
+    variasClasses.forEach(Element =>{
+        Element.classList.remove('border')
+        Element.classList.remove('border-4')
+        Element.classList.remove('border-light')
+
+    })
+
+    link.forEach(Element =>{
+        Element.classList.remove('text-white')
+        Element.classList.add('text-dark')
+    })
+} 
